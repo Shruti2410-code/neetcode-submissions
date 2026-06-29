1c8c98f4599e755eea4840f6e28b3adde3549d99
Shruti Prajapati <shrutiprajapati104@gmail.com>
2026-06-29 06:53:31 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s2.length();
+        int k = s1.length();
+        int[] freq1 = new int[26];
+        for(int i = 0; i < k; i++) freq1[s1.charAt(i)-'a']++;
+         int[] freq2 = new int[26];
+        int i = 0, j = 0;
+        while (j < n) {
+            char ch = s2.charAt(j);
+            freq2[ch - 'a']++;
+
+            if (j - i + 1 < k)
+                j++;
+            else if (j - i + 1 == k) {
+                if(Arrays.equals(freq1, freq2)) return true;
+                freq2[s2.charAt(i)-'a']--;
+                i++;
+                j++;
+                
+            }
+            
+        }
+        return false;
+    }
+}
